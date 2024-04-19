93641d93b5f7df17bebe62eddc1254e44b2b718e
Karim <m.karim.mimoune@gmail.com>
2024-04-19 09:45:50 +0200

    initial commit

---
+++ src/main/java/fr/eql/ai115/cda/hotel/paris/blue/app/AppApplication.java
@@ -0,0 +1,13 @@
+package fr.eql.ai115.cda.hotel.paris.blue.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eql/ai115/cda/hotel/paris/blue/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eql.ai115.cda.hotel.paris.blue.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
